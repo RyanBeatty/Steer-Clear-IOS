794783ef64a26b8e558df1def160a30442cffe2a
Ulises Giacoman <Ulises@UlisesM.local>
2015-07-25 21:40:16 -0400

    register and login

--- Steer Clear/ViewController.swift
+++ Steer Clear/ViewController.swift
@@ -7,50 +7,23 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func registerButton(sender: AnyObject) {
+        let registerController = register()
+        registerController.sendRequest()
+    }
+    
+    @IBAction func loginButton(sender: AnyObject) {
+        let loginController = login()
+        loginController.sendRequest()
+    }
+    
     @IBAction func hailARideButton(sender: AnyObject) {
         
-        let myUrl = NSURL(string: "http://127.0.0.1:5000/rides");
-        let request = NSMutableURLRequest(URL:myUrl!);
-        request.HTTPMethod = "POST";
-        
-        // Compose a query string
-        let postString = "phone_number=aaa-aaa-aaaa&num_passengers=4&start_latitude=10.0&start_longitude=50.1&end_latitude=5.03&end_longitude=1.04&id=5";
-        
-        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
-        
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
-            data, response, error in
-            
-            if error != nil
-            {
-                println("error=\(error)")
-                return
-            }
-            
-            // You can print out response object
-            println("response = \(response)")
-            
-            // Print out response body
-            let responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
-            println("responseString = \(responseString)")
-            
-            //Convert response to NSDictionary object:
-            
-            var err: NSError?
-            var myJSON = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error:&err) as? NSDictionary
-            
-            if let parseJSON = myJSON {
-                // TODO: work on parsing response
-//                var id = parseJSON["id"] as? String
-                println("Posted ahahaha")
-            }
-            
-        }
-        
-        task.resume()
     }
 
     override func viewDidLoad() {
@@ -64,5 +37,157 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+class register {
+    func sendRequest() {
+        /* Configure session, choose between:
+        * defaultSessionConfiguration
+        * ephemeralSessionConfiguration
+        * backgroundSessionConfigurationWithIdentifier:
+        And set session-wide properties, such as: HTTPAdditionalHeaders,
+        HTTPCookieAcceptPolicy, requestCachePolicy or timeoutIntervalForRequest.
+        */
+        let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        /* Create session, and optionally set a NSURLSessionDelegate. */
+        let session = NSURLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        
+        /* Create the Request:
+        My API (POST http://127.0.0.1:5000/register)
+        */
+        
+        var URL = NSURL(string: "http://127.0.0.1:5000/register")
+        let request = NSMutableURLRequest(URL: URL!)
+        request.HTTPMethod = "POST"
+        
+        // Form URL-Encoded Body
+        
+        let bodyParameters = [
+            "username": "smurf",
+            "password": "dog",
+        ]
+        let bodyString = self.stringFromQueryParameters(bodyParameters)
+        request.HTTPBody = bodyString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+        
+        /* Start a new Task */
+        let task = session.dataTaskWithRequest(request, completionHandler: { (data : NSData!, response : NSURLResponse!, error : NSError!) -> Void in
+            if (error == nil) {
+                // Success
+                let statusCode = (response as! NSHTTPURLResponse).statusCode
+                println("URL Session Task Succeeded: HTTP \(statusCode)")
+            }
+            else {
+                // Failure
+                println("URL Session Task Failed: %@", error.localizedDescription);
+            }
+        })
+        task.resume()
+    }
+    
+    /**
+    This creates a new query parameters string from the given NSDictionary. For
+    example, if the input is @{@"day":@"Tuesday", @"month":@"January"}, the output
+    string will be @"day=Tuesday&month=January".
+    @param queryParameters The input dictionary.
+    @return The created parameters string.
+    */
+    func stringFromQueryParameters(queryParameters : Dictionary<String, String>) -> String {
+        var parts: [String] = []
+        for (name, value) in queryParameters {
+            var part = NSString(format: "%@=%@",
+                name.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!,
+                value.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)
+            parts.append(part as String)
+        }
+        return "&".join(parts)
+    }
+    
+    /**
+    Creates a new URL by adding the given query parameters.
+    @param URL The input URL.
+    @param queryParameters The query parameter dictionary to add.
+    @return A new NSURL.
+    */
+    func NSURLByAppendingQueryParameters(URL : NSURL!, queryParameters : Dictionary<String, String>) -> NSURL {
+        let URLString : NSString = NSString(format: "%@?%@", URL.absoluteString!, self.stringFromQueryParameters(queryParameters))
+        return NSURL(string: URLString as String)!
+    }
+}
+
+class login {
+    func sendRequest() {
+        /* Configure session, choose between:
+        * defaultSessionConfiguration
+        * ephemeralSessionConfiguration
+        * backgroundSessionConfigurationWithIdentifier:
+        And set session-wide properties, such as: HTTPAdditionalHeaders,
+        HTTPCookieAcceptPolicy, requestCachePolicy or timeoutIntervalForRequest.
+        */
+        let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        /* Create session, and optionally set a NSURLSessionDelegate. */
+        let session = NSURLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        
+        /* Create the Request:
+        My API (2) (POST http://127.0.0.1:5000/login)
+        */
+        
+        var URL = NSURL(string: "http://127.0.0.1:5000/login")
+        let request = NSMutableURLRequest(URL: URL!)
+        request.HTTPMethod = "POST"
+        
+        // Form URL-Encoded Body
+        
+        let bodyParameters = [
+            "username": "smurfs",
+            "password": "dog",
+        ]
+        let bodyString = self.stringFromQueryParameters(bodyParameters)
+        request.HTTPBody = bodyString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+        
+        /* Start a new Task */
+        let task = session.dataTaskWithRequest(request, completionHandler: { (data : NSData!, response : NSURLResponse!, error : NSError!) -> Void in
+            if (error == nil) {
+                // Success
+                let statusCode = (response as! NSHTTPURLResponse).statusCode
+                println("URL Session Task Succeeded: HTTP \(statusCode)")
+            }
+            else {
+                // Failure
+                println("URL Session Task Failed: %@", error.localizedDescription);
+            }
+        })
+        task.resume()
+    }
+    
+    /**
+    This creates a new query parameters string from the given NSDictionary. For
+    example, if the input is @{@"day":@"Tuesday", @"month":@"January"}, the output
+    string will be @"day=Tuesday&month=January".
+    @param queryParameters The input dictionary.
+    @return The created parameters string.
+    */
+    func stringFromQueryParameters(queryParameters : Dictionary<String, String>) -> String {
+        var parts: [String] = []
+        for (name, value) in queryParameters {
+            var part = NSString(format: "%@=%@",
+                name.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!,
+                value.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)
+            parts.append(part as String)
+        }
+        return "&".join(parts)
+    }
+    
+    /**
+    Creates a new URL by adding the given query parameters.
+    @param URL The input URL.
+    @param queryParameters The query parameter dictionary to add.
+    @return A new NSURL.
+    */
+    func NSURLByAppendingQueryParameters(URL : NSURL!, queryParameters : Dictionary<String, String>) -> NSURL {
+        let URLString : NSString = NSString(format: "%@?%@", URL.absoluteString!, self.stringFromQueryParameters(queryParameters))
+        return NSURL(string: URLString as String)!
+    }
 }
 
