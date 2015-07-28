58928abc234627a8e5efa493e8ad53c26b3c456b
Ulises Giacoman <ugiacoman@email.wm.edu>
2015-07-28 14:03:59 -0400

    fully searchable location with bias in the burg, will search for establishments and geolocation

---
+++ Steer Clear/MapTasks.swift
@@ -0,0 +1,13 @@
+//
+//  MapTasks.swift
+//  Steer Clear
+//
+//  Created by Ulises Giacoman on 7/28/15.
+//  Copyright (c) 2015 Paradoxium. All rights reserved.
+//
+
+import UIKit
+
+class MapTasks: NSObject {
+   
+}
