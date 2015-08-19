0b30d67fd9ff9d4cb6d5e95c327ec77b384d0d53
Ryan Beatty <rvbeatty@email.wmd.edu>
2015-08-18 20:47:33 -0400

    rewrote 1 test

--- Steer ClearTests/Steer_ClearTests.swift
+++ Steer ClearTests/Steer_ClearTests.swift
@@ -9,8 +9,11 @@
 import UIKit
 import XCTest
 import Foundation
+import OHHTTPStubs
 
 class Steer_ClearTests: XCTestCase {
+    
+    let SERVER_HOSTNAME = "127.0.0.1"
     
     override func setUp() {
         super.setUp()
@@ -21,82 +24,118 @@ class Steer_ClearTests: XCTestCase {
         super.tearDown()
     }
     
-    func testSuccessfulRegisterandConflict() {
-        var networkController = Network()
-        var responso = 0
-        let username = "ugiacoman"
-        let password = ""
-        let phone =  "7036786244"
-        
-        
-        // Successful Register Test
-        print("testSuccessfulRegisterandConflict info: username: \(username), password: \(password), phone: \(phone) \n")
-        networkController.register(username as String, password: password as String, phone: phone)
-        while (networkController.responseFound != true){
-            usleep(3000)
-        }
-        networkController.responseFound = false
-        responso = networkController.responseStatus
-        XCTAssertEqual(responso, 200)
-        print("Unique Register Successfull \n")
-        
-        // Conflict Register Test
-        networkController.register(username as String, password: password as String, phone: phone)
-        while (networkController.responseFound != true){
-            usleep(3000)
-        }
-        networkController.responseFound = false
-        responso = networkController.responseStatus
-        XCTAssertEqual(responso, 409)
-        print("Conflict Submission Successfull \n")
+    func _test(request: NSURLRequest) -> Bool {
+        return true
     }
     
-//    func testLogout() {
+    func _testresp(request: NSURLRequest) -> OHHTTPStubsResponse {
+        let stubData = "Hello, World!".dataUsingEncoding(NSUTF8StringEncoding)
+        return OHHTTPStubsResponse(data: stubData!, statusCode: 200, headers: nil)
+    }
+    
+    func testRegisterSuccess() {
+        OHHTTPStubs.stubRequestsPassingTest({
+            request in
+            return request.URL!.host == self.SERVER_HOSTNAME
+        }, withStubResponse: {
+            _ in
+            let stubData = "Hello, World!".dataUsingEncoding(NSUTF8StringEncoding)
+            return OHHTTPStubsResponse(data: stubData!, statusCode: 200,headers: nil)
+        })
+        
+//        OHHTTPStubs.stubRequestsPassingTest(_test, withStubResponse: _testresp)
+        
+        
+        let username = "user"
+        let password = "password"
+        let phone = "+12223334444"
+        
+        var connection = Network()
+        connection.register(username, password: password, phone: phone)
+        
+        while (connection.responseFound != true){
+            usleep(3000)
+        }
+        
+        XCTAssertEqual(connection.responseStatus, 200)
+    }
+//    
+//    func testSuccessfulRegisterandConflict() {
 //        var networkController = Network()
 //        var responso = 0
-//        let email = randomStringWithLength(5)
-//        let password = randomStringWithLength(5)
-//        let phone =  "703" + (randomIntWithLength(7) as String)
-//        networkController.register(email as String, password: password as String, phone: phone)
-//        print("testLogout info: email: \(email), password: \(password), phone: \(invalidPhone) \n")
+//        let username = "ugiacoman"
+//        let password = ""
+//        let phone =  "7036786244"
+//        
+//        
+//        // Successful Register Test
+//        print("testSuccessfulRegisterandConflict info: username: \(username), password: \(password), phone: \(phone) \n")
+//        networkController.register(username as String, password: password as String, phone: phone)
+//        while (networkController.responseFound != true){
+//            usleep(3000)
+//        }
+//        networkController.responseFound = false
+//        responso = networkController.responseStatus
 //        XCTAssertEqual(responso, 200)
+//        print("Unique Register Successfull \n")
+//        
+//        // Conflict Register Test
+//        networkController.register(username as String, password: password as String, phone: phone)
+//        while (networkController.responseFound != true){
+//            usleep(3000)
+//        }
+//        networkController.responseFound = false
+//        responso = networkController.responseStatus
+//        XCTAssertEqual(responso, 409)
+//        print("Conflict Submission Successfull \n")
 //    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-    func randomStringWithLength (len : Int) -> NSString {
-        
-        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        
-        var randomString : NSMutableString = NSMutableString(capacity: len)
-        
-        for (var i=0; i < len; i++){
-            var length = UInt32 (letters.length)
-            var rand = arc4random_uniform(length)
-            randomString.appendFormat("%C", letters.characterAtIndex(Int(rand)))
-        }
-        
-        return randomString
-    }
-    
-    func randomIntWithLength (len : Int) -> NSString {
-        
-        let letters : NSString = "0123456789"
-        
-        var randomString : NSMutableString = NSMutableString(capacity: len)
-        
-        for (var i=0; i < len; i++){
-            var length = UInt32 (letters.length)
-            var rand = arc4random_uniform(length)
-            randomString.appendFormat("%C", letters.characterAtIndex(Int(rand)))
-        }
-        
-        return randomString
-    }
+//    
+////    func testLogout() {
+////        var networkController = Network()
+////        var responso = 0
+////        let email = randomStringWithLength(5)
+////        let password = randomStringWithLength(5)
+////        let phone =  "703" + (randomIntWithLength(7) as String)
+////        networkController.register(email as String, password: password as String, phone: phone)
+////        print("testLogout info: email: \(email), password: \(password), phone: \(invalidPhone) \n")
+////        XCTAssertEqual(responso, 200)
+////    }
+//    
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock() {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+//    
+//    func randomStringWithLength (len : Int) -> NSString {
+//        
+//        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+//        
+//        var randomString : NSMutableString = NSMutableString(capacity: len)
+//        
+//        for (var i=0; i < len; i++){
+//            var length = UInt32 (letters.length)
+//            var rand = arc4random_uniform(length)
+//            randomString.appendFormat("%C", letters.characterAtIndex(Int(rand)))
+//        }
+//        
+//        return randomString
+//    }
+//    
+//    func randomIntWithLength (len : Int) -> NSString {
+//        
+//        let letters : NSString = "0123456789"
+//        
+//        var randomString : NSMutableString = NSMutableString(capacity: len)
+//        
+//        for (var i=0; i < len; i++){
+//            var length = UInt32 (letters.length)
+//            var rand = arc4random_uniform(length)
+//            randomString.appendFormat("%C", letters.characterAtIndex(Int(rand)))
+//        }
+//        
+//        return randomString
+//    }
     
 }
